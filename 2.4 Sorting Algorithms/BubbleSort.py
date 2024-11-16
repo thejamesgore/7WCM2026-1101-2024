d047aa732f01fe7e7ffc5508d20d6ae092f9a762
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-11-16 12:19:25 +0000

    Fixing bubble sort bug

--- 2.4 Sorting Algorithms/BubbleSort.py
+++ 2.4 Sorting Algorithms/BubbleSort.py
@@ -18,7 +18,7 @@ def bubble_sort_by_length(list):
         has_swapped = False
         for i in range(len(list) -1):
             # print("DO WE NEED TO SWAP ??", list[i], list[i + 1])
-            if len(list[i]) > len(list[i + 1]):
+            if len(list[i]) > len(list[i + 1]): # notable issue when comparing final two items in list. If I use < or > then it will incorrectly state that Canterbury is of a shorter length than Antwerp
                 list[i], list[i + 1] = list[i + 1], list[i]
                 # print("YES, WE JUST DID A SWAP !!", list[i], list[i + 1])
                 has_swapped = True
@@ -31,7 +31,7 @@ def bubble_sort_by_length(list):
         
     print("The newly sorted list is: ", list)
 
-bubble_sort_by_length(cities)
+# bubble_sort_by_length(cities)
 
 
 def bubble_sort_alphabetical(list):
@@ -53,6 +53,7 @@ def bubble_sort_alphabetical(list):
             if len(left[-1]) < len(right[-1]):
                 list[i], list[i + 1] = list[i + 1], list[i]
                 has_swapped = True
+            i = i + 1
         i = 0
         
     print("The newly sorted list in alphabetical order is :", list)
