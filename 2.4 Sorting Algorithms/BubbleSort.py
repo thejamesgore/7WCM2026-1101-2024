6e5e2327a073d77a42ba56428de7394901242ffc
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-11-16 12:32:44 +0000

    Add: bubble sort integers function

--- 2.4 Sorting Algorithms/BubbleSort.py
+++ 2.4 Sorting Algorithms/BubbleSort.py
@@ -2,6 +2,8 @@
 #date: November 2024
 # This function will sort a list in ascending order. The list can be integers, or words. If words it can sort them either by length or alphabetical order
 
+import random
+
 cities = ["London", "Canterbury", "York", "Leicester", "Antwerp"]
 
 
@@ -34,6 +36,33 @@ def bubble_sort_by_length(list):
 
 # bubble_sort_by_length(cities)
 
+random_list = [random.randint(1, 100) for _ in range(10)] 
+
+
+def bubble_sort_integers(list):
+    print("Lets sort these integers in ascending order!")
+    print("   ")
+    print("   ")
+    print("We're going to sort the following: ", list)
+    
+    i = 0
+    has_swapped = True
+    
+    while has_swapped == True:
+        has_swapped = False
+        
+        for i in range(len(list) -1):
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                has_swapped = True
+        
+        i = i + 1
+    i = 0
+    
+    print("Our newly ordered list of integers is...", list)
+    
+bubble_sort_integers(random_list)
+
 
 def bubble_sort_alphabetical(list):
     print("Lets sort alphabetically then!")
@@ -60,4 +89,4 @@ def bubble_sort_alphabetical(list):
     print("The newly sorted list in alphabetical order is :", list)
             
             
-bubble_sort_alphabetical(cities)
+# bubble_sort_alphabetical(cities)
