bce4c6dca1412a6ef893c3705932923d7868c44e
James Gore <83005220+thejamesgore@users.noreply.github.com>
2024-11-22 22:47:06 +0000

    Add: matrix multi with numpy

--- 3.3 NumPy & Matolib/numpy-task.py
+++ 3.3 NumPy & Matolib/numpy-task.py
@@ -7,9 +7,17 @@
 
 import numpy as np
 
-matrix_a = np.random.randint(-10,10,(3,6))
+matrix_a = np.random.randint(-10,10,(2,2))
 
 matrix_t = np.transpose(matrix_a)
 
 print("The Matrix is", matrix_a)
 print("The Matrix transposed is", matrix_t)
+
+matrix_c = np.matmul(matrix_a,matrix_t)
+
+matrix_d = np.matmul(matrix_t,matrix_a)
+
+print("The matricies multiplied are: A * A^T", matrix_c)
+print("     ")
+print("The matricies multiplied are: A^T * A", matrix_d)
